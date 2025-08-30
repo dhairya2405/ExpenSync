e23ac65fa240c1d7cb199a9ecb22a95ec0364bd7
dhairya2405 <gandhidhairya2405@gmail.com>
2025-08-30 14:18:05 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,14 @@ const dashboardRoutes = require("./routes/dashboardRoutes");
 const app = express();
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+  origin: process.env.CLIENT_URL || "https://expen-sync.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // must include OPTIONS
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 }));
+
+// Handle preflight requests
+app.options("*", cors());
 
 app.use(express.json()); // Parse JSON body
 
